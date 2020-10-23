b916cbad21cb16895e38f684f43b71816d798daf
Keeper991 <nesmile23@gmail.com>
2020-10-24 04:10:41 +0900

    add passport-local-mongoose strategy

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import routes from "../routes";
+import User from "../models/User";
 
-export const join = (req, res) => {
+export const join = async (req, res) => {
   if (req.method === "GET") {
     res.render("join", { pageTitle: "Join" });
   } else if (req.method === "POST") {
@@ -12,6 +13,15 @@ export const join = (req, res) => {
       res.render("join", { pageTitle: "Join" });
     } else {
       // To Do : Register user
+      try {
+        const user = await User.create({
+          name,
+          email,
+        });
+        await User.register(user, password);
+      } catch (error) {
+        console.log(error);
+      }
       // To Do : Log user in
       res.redirect(routes.home);
     }
