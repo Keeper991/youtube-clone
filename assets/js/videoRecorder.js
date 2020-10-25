ae61548d70f533289fa601b975b0f588e926a347
Keeper991 <nesmile23@gmail.com>
2020-10-25 23:47:42 +0900

    add recording feature

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,21 +2,54 @@ const recordContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const recordPreview = document.getElementById("jsRecordPreview");
 
-const startRecording = async () => {
+let stream;
+let videoRecorder;
+
+const handleVideoData = (e) => {
+  const { data: videoFile } = e;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "record.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.innerHTML = "Start Recording";
+  // eslint-disable-next-line no-use-before-define
+  recordBtn.addEventListener("click", startRecording);
+  recordBtn.removeEventListener("click", stopRecording);
+};
+
+const startRecording = () => {
+  videoRecorder = new MediaRecorder(stream);
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.removeEventListener("click", startRecording);
+  recordBtn.addEventListener("click", stopRecording);
+  recordBtn.innerHTML = "Stop Recording";
+  videoRecorder.start();
+};
+
+const getVideo = async () => {
   try {
-    const stream = await navigator.mediaDevices.getUserMedia({
+    stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
       video: { width: 1280, height: 720 },
     });
     recordPreview.srcObject = stream;
     recordPreview.play();
+    recordBtn.innerHTML = "Start Recording";
+    recordBtn.addEventListener("click", startRecording);
   } catch (error) {
     recordBtn.innerHTML = "😢 Cant record";
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
 const init = () => {
-  startRecording();
+  recordBtn.addEventListener("click", getVideo);
 };
 
 if (recordContainer) {
